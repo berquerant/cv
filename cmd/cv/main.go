cb97bf7790c4b30489da92ff6de119aa8e0bbf47
berquerant <sin@sin-pro.local>
2023-10-21 02:17:57 +0900

    -i, -o options as positional arguments

--- cmd/cv/main.go
+++ cmd/cv/main.go
@@ -12,25 +12,36 @@ import (
 func main() {
 	var (
 		delimiter = flag.String("d", ",", "delimiter")
-		src       = flag.String("i", "auto", "source format")
-		dst       = flag.String("o", "json", "destination format")
 	)
 	flag.Usage = Usage
 	flag.Parse()
+	args := flag.Args()
+
+	if len(args) != 2 {
+		fail("input format and output format are required")
+	}
+
+	var (
+		src = cv.TypeFromString(args[0])
+		dst = cv.TypeFromString(args[1])
+	)
 
 	input, err := io.ReadAll(os.Stdin)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
-		os.Exit(1)
+		fail("%v\n", err)
 	}
 
-	c := cv.New(cv.TypeFromString(*src), cv.TypeFromString(*dst), []rune(*delimiter)[0])
+	c := cv.New(src, dst, []rune(*delimiter)[0])
 	result, err := c.Translate(input)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
-		os.Exit(1)
+		fail("%v\n", err)
 	}
 	fmt.Printf("%s", result)
+}
+
+func fail(format string, v ...any) {
+	fmt.Fprintf(os.Stderr, format, v...)
+	os.Exit(1)
 }
 
 func Usage() {
@@ -41,12 +52,10 @@ func Usage() {
 const usage = `cv - Translate data
 
 Usage:
-  cv [flags]
+  cv INPUT_FORMAT OUTPUT_FORMAT [flags]
 
 Read data from standard input and convert it to the specified format, then write it to standard output.
 
-The input format is specified with the -i option. If it is not specified, it will be automatically determined.
-The output format is specified with the -o option. If it is not specified, it will be json.
 Valid values for each format are json, yaml, toml, and csv.
 For csv format, the delimiter can be specified with the -d option. If it is not specified, it will be , (comma).
 `
